118bf317e664d8d810f009156d887de4258a28cc
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-08-01 19:46:42 -0300

    a

--- prompter/prompter/items.py
+++ prompter/prompter/items.py
@@ -3,6 +3,23 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+
+
+
+
+
+
+#quitar ccuando el proyeto este finalizado
+
+#mgpeirone25@gmail.com
+#DINOsaurio2008
+
+#ID de input  = prompt-textarea
+
+#document.getElementById('prompt-textarea').innerText = "hola"
+
+
+
 import scrapy
 
 
